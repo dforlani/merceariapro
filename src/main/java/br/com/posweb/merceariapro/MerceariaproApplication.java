a9a26a259eec1bb0239ec8933bcce84dc5ff9f27
Diogo <dforlani@gmail.com>
2021-03-23 21:59:22 -0300

    first commit

---
+++ src/main/java/br/com/posweb/merceariapro/MerceariaproApplication.java
@@ -0,0 +1,13 @@
+package br.com.posweb.merceariapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerceariaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerceariaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/posweb/merceariapro/MerceariaproApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.posweb.merceariapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerceariaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
